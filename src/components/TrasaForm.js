9b42df6b1ced6db434a17405c9fcc97cddb585bf
cinek5 <marcinkrawczyk97@hotmail.com>
2019-01-05 22:17:23 +0100

    dodany formularz dodawania nowej trasy

---
+++ src/components/TrasaForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { MdDelete } from "react-icons/md";
+import { MdBuild } from "react-icons/md";
+
+
+class TrasaPresent extends React.Component {
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default TrasaPresent;
